501c250ea5f392d7e0372ba603c1a1ec3291b0d4
VillainLouis <115220933+VillainLouis@users.noreply.github.com>
2022-10-11 20:08:39 +0800

    Create nfs.sh

---
+++ nfs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+apt-get install nfs-common
+mkdir -p /data/zwyao/cross_FL
+mount -t nfs 172.16.62.107:/data/zwyao/cross_FL /data/zwyao/cross_FL
+
+cat >> /etc/fstab << EOF
+172.16.62.107:/data/zwyao/cross_FL /data/zwyao/cross_FL  nfs
+EOF
+df -h
+echo "if you can see the mounted directories, then it's well done"
