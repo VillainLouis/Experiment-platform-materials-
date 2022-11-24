43d2da56c260de3ae59c02d8a417ce9a4ef1c79e
VillainLouis <115220933+VillainLouis@users.noreply.github.com>
2022-11-24 17:13:34 +0800

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -3,14 +3,12 @@
 
 apt-get install nfs-common
 mkdir -p /data/zwyao/cross_FL
-mount -t nfs 172.16.62.107:/data/zwyao/cross_FL /data/zwyao/cross_FL
+mount -t nfs 172.16.63.64:/data/zwyao/cross_FL /data/zwyao/cross_FL
 
 cat >> /etc/fstab << EOF
-172.16.62.107:/data/zwyao/cross_FL /data/zwyao/cross_FL  nfs
+172.16.63.64:/data/zwyao/cross_FL /data/zwyao/cross_FL  nfs
 EOF
 df -h
-echo "if you can see the mounted directories, then it's well done"
+echo "If you can see the mounted directories, then it's well done"
 
-# echo "pulling docker image from Docker hub"
-# docker pull yaozhiwei/mpinode:1.0
 
