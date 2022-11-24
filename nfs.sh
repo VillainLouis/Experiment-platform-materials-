37f913cc9bd2350448f740bbe0b7c8a850ba4833
VillainLouis <115220933+VillainLouis@users.noreply.github.com>
2022-11-24 18:19:50 +0800

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -12,3 +12,7 @@ df -h
 echo "If you can see the mounted directories, then it's well done"
 
 
+# Oneline
+apt-get install nfs-common && mkdir -p /data/zwyao/cross_FL && mount -t nfs 172.16.63.64:/data/zwyao/cross_FL /data/zwyao/cross_FL && cat >> /etc/fstab << EOF 172.16.63.64:/data/zwyao/cross_FL /data/zwyao/cross_FL  nfs EOF && df -h && echo "If you can see the mounted directories, then it's well done"
+
+
